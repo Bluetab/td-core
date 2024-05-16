0ab471bd6a486f9a05fba94420ffa117bd1c6933
Jandro Oliva <alejandro.oliva@bluetab.net>
2024-05-16 09:21:20 +0200

    chore: TD-6499 update mix with new td-df-lib version

--- mix.exs
+++ mix.exs
@@ -38,7 +38,8 @@ defmodule TdCore.MixProject do
         git: "https://github.com/Bluetab/td-cluster.git", tag: "5.19.0"
       },
       {:td_cache, git: "https://github.com/Bluetab/td-cache.git", tag: "6.3.0"},
-      {:td_df_lib, git: "https://github.com/Bluetab/td-df-lib.git", tag: "6.5.1"}
+      {:td_df_lib,
+       git: "https://github.com/Bluetab/td-df-lib.git", branch: "feature/td-6499", override: true}
     ]
   end
 end
