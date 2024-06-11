f4d1e6a77085e3e69440d865135e7fb93ccf23a9
Juan Francisco Huete Verdejo <jf.huete@bluetab.net>
2024-06-11 15:54:21 +0200

    chore: TD-6440 Bump td-df-lib version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule TdCore.MixProject do
   def project do
     [
       app: :td_core,
-      version: "6.5.3",
+      version: "6.7.0",
       elixir: "~> 1.14",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
@@ -38,7 +38,7 @@ defmodule TdCore.MixProject do
         git: "https://github.com/Bluetab/td-cluster.git", tag: "5.19.0"
       },
       {:td_cache, git: "https://github.com/Bluetab/td-cache.git", tag: "6.3.0"},
-      {:td_df_lib, git: "https://github.com/Bluetab/td-df-lib.git", branch: "bugfix/td-6440"}
+      {:td_df_lib, git: "https://github.com/Bluetab/td-df-lib.git", tag: "6.7.0"}
     ]
   end
 end
