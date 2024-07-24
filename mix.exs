9bf6a0b7144dd82b3ae8666870d20a922e4fd33a
Jandro Oliva <alejandro.oliva@bluetab.net>
2024-07-24 12:20:13 +0200

    chore: TD-6689 update td-cache and td-df-lib

--- mix.exs
+++ mix.exs
@@ -37,8 +37,8 @@ defmodule TdCore.MixProject do
         :td_cluster,
         git: "https://github.com/Bluetab/td-cluster.git", tag: "5.19.0"
       },
-      {:td_cache, git: "https://github.com/Bluetab/td-cache.git", tag: "6.9.1"},
-      {:td_df_lib, git: "https://github.com/Bluetab/td-df-lib.git", tag: "6.9.2"}
+      {:td_cache, git: "https://github.com/Bluetab/td-cache.git", tag: "6.9.2"},
+      {:td_df_lib, git: "https://github.com/Bluetab/td-df-lib.git", tag: "6.9.3"}
     ]
   end
 end
