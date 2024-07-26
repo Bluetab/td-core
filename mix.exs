dc33a354a303ceaaa2ea2a25be9383219d279643
Jandro Oliva <alejandro.oliva@bluetab.net>
2024-07-26 14:15:29 +0200

    fix: TD-6733 update td-df-lib

--- mix.exs
+++ mix.exs
@@ -38,7 +38,7 @@ defmodule TdCore.MixProject do
         git: "https://github.com/Bluetab/td-cluster.git", tag: "5.19.0"
       },
       {:td_cache, git: "https://github.com/Bluetab/td-cache.git", tag: "6.9.2"},
-      {:td_df_lib, git: "https://github.com/Bluetab/td-df-lib.git", tag: "6.9.4"}
+      {:td_df_lib, git: "https://github.com/Bluetab/td-df-lib.git", tag: "6.9.5"}
     ]
   end
 end
