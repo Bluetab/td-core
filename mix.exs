86aca0d19500445f3b605d2172f16258e39ee079
Guillermo Rodriguez <guillermo.r.navarrete@bluetab.net>
2025-06-20 14:25:22 +0200

    chore: TD-7300 bump version 7.7.0

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule TdCore.MixProject do
   def project do
     [
       app: :td_core,
-      version: "7.6.0",
+      version: "7.7.0",
       elixir: "~> 1.18",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
@@ -34,8 +34,8 @@ defmodule TdCore.MixProject do
       {:elasticsearch, git: "https://github.com/Bluetab/elasticsearch-elixir.git"},
       {:credo, "~> 1.7.11", only: [:dev, :test], runtime: false},
       {:td_cluster, git: "https://github.com/Bluetab/td-cluster.git", tag: "7.5.0"},
-      {:td_cache, git: "https://github.com/Bluetab/td-cache.git", branch: "feature/td-7300"},
-      {:td_df_lib, git: "https://github.com/Bluetab/td-df-lib.git", branch: "feature/td-7300"}
+      {:td_cache, git: "https://github.com/Bluetab/td-cache.git", tag: "7.7.0"},
+      {:td_df_lib, git: "https://github.com/Bluetab/td-df-lib.git", tag: "7.7.0"}
     ]
   end
 end
