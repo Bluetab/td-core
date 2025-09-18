ccb47938c92bcfcaf393f2444dc7d5f21548e622
Guillermo Rodriguez <guillermo.r.navarrete@bluetab.net>
2025-09-18 16:17:29 +0200

    chore: TD-7301 td-cluster update lib

--- mix.exs
+++ mix.exs
@@ -33,13 +33,9 @@ defmodule TdCore.MixProject do
       {:mox, "~> 1.2", only: :test},
       {:elasticsearch, git: "https://github.com/Bluetab/elasticsearch-elixir.git"},
       {:credo, "~> 1.7.11", only: [:dev, :test], runtime: false},
-      {:td_cluster, git: "https://github.com/Bluetab/td-cluster.git", tag: "7.10.1"},
+      {:td_cluster, git: "https://github.com/Bluetab/td-cluster.git", tag: "7.10.2"},
       {:td_cache, git: "https://github.com/Bluetab/td-cache.git", branch: "feature/td-7301"},
       {:td_df_lib, git: "https://github.com/Bluetab/td-df-lib.git", branch: "feature/td-7301"}
-
-
-
-
     ]
   end
 end
