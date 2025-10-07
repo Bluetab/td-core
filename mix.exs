e5bcde2a082eb2b61fa295f86632ec54a6fb78c6
saul.isern <saul.isern@bluetab.net>
2025-10-07 14:41:49 +0200

    feat: update dependencies

--- mix.exs
+++ mix.exs
@@ -34,8 +34,8 @@ defmodule TdCore.MixProject do
       {:elasticsearch, git: "https://github.com/Bluetab/elasticsearch-elixir.git"},
       {:credo, "~> 1.7.11", only: [:dev, :test], runtime: false},
       {:td_cluster, git: "https://github.com/Bluetab/td-cluster.git", tag: "7.10.2"},
-      {:td_cache, git: "https://github.com/Bluetab/td-cache.git", tag: "7.11.1"},
-      {:td_df_lib, git: "https://github.com/Bluetab/td-df-lib.git", tag: "7.11.1"}
+      {:td_cache, git: "https://github.com/Bluetab/td-cache.git", branch: "feature/td-7340"},
+      {:td_df_lib, git: "https://github.com/Bluetab/td-df-lib.git", branch: "feature/td-7340"}
     ]
   end
 end
