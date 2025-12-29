3bf69e3ce22307ba9f21e24c3774a1864327237c
saul.isern <saul.isern@bluetab.net>
2025-12-29 15:00:06 +0100

    feat: fetch quality controls from storeç

--- lib/td_core/search/store.ex
+++ lib/td_core/search/store.ex
@@ -7,4 +7,9 @@ defmodule TdCore.Search.Store do
     {:ok, data} = TdCluster.TdBg.Store.fetch(ids)
     data
   end
+
+  def fetch(:quality_controls, ids) do
+    {:ok, data} = TdCluster.TdQx.Store.fetch(quality_control_ids: ids)
+    data
+  end
 end
