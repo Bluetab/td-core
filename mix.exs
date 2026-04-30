0eafbbb933a8c5ced78b6090cc6c03085578b79b
Victor del Pino <victor.delpino@bluetab.net>
2026-04-30 10:41:29 +0200

    chore: TD-8083 update dependencies

--- mix.exs
+++ mix.exs
@@ -36,8 +36,8 @@ defmodule TdCore.MixProject do
       {:elasticsearch, git: "https://github.com/Bluetab/elasticsearch-elixir.git"},
       {:credo, "~> 1.7.11", only: [:dev, :test], runtime: false},
       {:td_cluster, git: "https://github.com/Bluetab/td-cluster.git", tag: "8.3.0"},
-      {:td_cache, git: "https://github.com/Bluetab/td-cache.git", tag: "8.3.0"},
-      {:td_df_lib, git: "https://github.com/Bluetab/td-df-lib.git", tag: "8.4.2"}
+      {:td_cache, git: "https://github.com/Bluetab/td-cache.git", branch: "feature/td-8083"},
+      {:td_df_lib, git: "https://github.com/Bluetab/td-df-lib.git", branch: "feature/td-8083"}
     ]
   end
 end
