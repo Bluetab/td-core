28189153d0b928f7d863f2bf54e93af59ddbc136
saul.isern <saul.isern@bluetab.net>
2026-06-10 11:39:31 +0200

    chore: TD-8168 bump versions

--- mix.exs
+++ mix.exs
@@ -4,11 +4,10 @@ defmodule TdCore.MixProject do
   def project do
     [
       app: :td_core,
-      version: "8.7.0",
+      version: "8.7.1",
       elixir: "~> 1.18",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
-      aliases: aliases(),
       deps: deps()
     ]
   end
@@ -36,15 +35,9 @@ defmodule TdCore.MixProject do
       {:xlsx_reader, "~> 0.8.11"},
       {:elasticsearch, git: "https://github.com/Bluetab/elasticsearch-elixir.git", tag: "8.6.0"},
       {:credo, "~> 1.7.11", only: [:dev, :test], runtime: false},
-      {:td_cluster, git: "https://github.com/Bluetab/td-cluster.git", tag: "8.3.0"},
-      {:td_cache, git: "https://github.com/Bluetab/td-cache.git", tag: "8.6.2"},
-      {:td_df_lib, git: "https://github.com/Bluetab/td-df-lib.git", tag: "8.6.3"}
-    ]
-  end
-
-  defp aliases do
-    [
-      quality: ["format --check-formatted", "credo --strict"]
+      {:td_cluster, git: "https://github.com/Bluetab/td-cluster.git", tag: "8.7.0"},
+      {:td_cache, git: "https://github.com/Bluetab/td-cache.git", tag: "8.7.0"},
+      {:td_df_lib, git: "https://github.com/Bluetab/td-df-lib.git", tag: "8.7.0"}
     ]
   end
 end
