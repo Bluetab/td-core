79845cc7953e7a13b19a5fad199c00e4ae718d0d
Jandro Oliva <alejandro.oliva@bluetab.net>
2026-08-07 13:49:57 +0200

    chore: bump version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule TdCore.MixProject do
   def project do
     [
       app: :td_core,
-      version: "8.9.1",
+      version: "8.10.0",
       elixir: "~> 1.18",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
